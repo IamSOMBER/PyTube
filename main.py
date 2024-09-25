9f7221a5df8bfd01a6e2cfb946b17aed4090ace3
Cruciani Mirko <cmirko@gmail.com>
2024-09-25 23:21:36 +0200

    Refactor video download process and update Python SDK.
    Separate video stream selection logic and streamline audio extraction decision. Updated project files to use Python 3.10 SDK for compatibility.

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 
-import pytube
-from moviepy.editor import VideoFileClip
 import os
 
+import pytube
+from moviepy.editor import VideoFileClip
 
-def download_stream(video_url):
+VIDEO_FORMAT = "acodec"
+DEFAULT_ITAG = 137
+AUDIO_OUTPUT_EXT = ".mp3"
+
+
+def show_available_video_streams(streams):
+    for this_stream in streams:
+        # mostra la lista dei flussi video disponibili
+        if VIDEO_FORMAT in str(this_stream):
+            print(this_stream)
+
+
+def get_stream_name_from_itag(video_url):
     videos = pytube.YouTube(video_url)
 
-    for stream in videos.streams:
-        # mostra la lista dei video disponibili con le caratteristiche rilevanti
-        if "video/mp4" in str(stream):
-            print(stream)
+    show_available_video_streams(videos.streams)
 
     # scegli dalla lista in base all' 'itag'
-    # itag = input("inserisci l'itag del video: ")
-    itag = 22
+    itag = input("inserisci l'itag del video: ")
     video = videos.streams.get_by_itag(int(itag))
 
     # rinomina video e scarica
@@ -29,11 +37,10 @@ def download_stream(video_url):
 
 
 def extract_audio_from_downloaded_video(video_downloaded):
-    output_ext = ".mp3"
     filename, ext = os.path.splitext(video_downloaded)
     video_clip = VideoFileClip(str(video_downloaded))
     audio_file = video_clip.audio
-    audio_file.write_audiofile(f"{filename}.{output_ext}")
+    audio_file.write_audiofile(f"{filename}.{AUDIO_OUTPUT_EXT}")
 
     # Close the video clip
     video_clip.close()
@@ -49,10 +56,9 @@ if __name__ == '__main__':
         stream_list.append(url)
 
     for stream in stream_list:
-        file_video_name = download_stream(stream)
-        # print(file_video_name)
+        file_video_name = get_stream_name_from_itag(stream)
+        print(file_video_name)
 
-        """ extract_audio = input("vuoi estrarre l'audio? (S/n) ")
-        if extract_audio.lower =='s' or extract_audio =="":
-            extract_audio_from_downloaded_video(file_video_name.replace("..", ".")) """
-        extract_audio_from_downloaded_video(file_video_name.replace("..", "."))
+        extract_audio = input("vuoi estrarre l'audio? (S/n) ")
+        if extract_audio.lower == 's' or extract_audio == "":
+            extract_audio_from_downloaded_video(file_video_name.replace("..", "."))
